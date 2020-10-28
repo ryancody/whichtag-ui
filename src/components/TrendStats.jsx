73dbb64d3f9c7b59ff9a5524925a61f25a8464d6
Ryan <8892867+ryancody@users.noreply.github.com>
2020-10-28 13:46:23 -0400

    switch to display whichtag score (#4)

--- src/components/TrendStats.jsx
+++ src/components/TrendStats.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
 
 function TrendStats(props) {
+    let tableRows = [...props.trends].sort((a,b) => b.score - a.score).map(trend => {
+        let emoji = '🥶'
 
-    let tableRows = [...props.trends].sort((a,b) => b.tweetsPerSecond - a.tweetsPerSecond).map(trend => {
+        if(trend.score > 90){
+            emoji = '🚀'
+        } else if (trend.score > 80) {
+            emoji = '🔥'
+        } else if (trend.score > 70) {
+            emoji = '😐'
+        }
 
-
-            return <tr className='is-size-4' key={trend.query}>
-                        <td>
-                            <div className='is-size-3'>{trend.emoji} {trend.query}</div>
-                            <div>
-                                    <span className='has-text-weight-bold has-text-primary'>{trend.data.tweets.length} {trend.tweetPlurality}</span> in
-                                    <span className='has-text-weight-bold has-text-primary'> {trend.timeQty} {trend.timeUnits}</span>
-                            </div>
-                            <div>
-                                <span role='img' aria-label='views'>👀</span><span role='img' aria-label='by'> ⨯ </span> 
-                                <span className='has-text-weight-bold has-text-primary'>{Number(trend.possibleViews).toLocaleString()} followers</span>
-                            </div>
-                        </td>
-                    </tr>
+        return <tr className='is-size-4' key={trend.query}>
+                    <td>
+                        <div className='has-text-weight-bold has-text-primary'>{emoji} {trend.query}</div>
+                    </td>
+                    <td>
+                        <div className='has-text-centered'>
+                            <span className='has-text-weight-bold has-text-primary'>{trend.score.toFixed(1)}</span>
+                        </div>
+                    </td>
+                </tr>
         })
+
+    let tableHead =                 
+        <thead>
+            <tr>
+                <td></td>
+                <td className='has-text-centered'>score</td>
+            </tr>
+        </thead>
+
+    if (tableRows.length === 0){
+        tableHead = null
+    }
 
     return(
         <table className='table'>
+            {tableHead}
             <tbody>
                 {tableRows}
             </tbody>
--- src/models/Trend.js
+++ src/models/Trend.js
@@ -5,10 +5,7 @@ class Trend {
         this.isComplete = false
         this.isSuccess = false
 
-        this.spanSeconds = 0
-        this.queryTime = Date()
-        this.oldestTweetCreatedAt = ''
-        this.possibleViews = 0
+        this.score = 0
 
         this.tweetsPerSecond = 0
     }
@@ -20,35 +17,10 @@ class Trend {
         }
 
         this.data = data
-        this.oldestTweetCreatedAt = Date.parse(data.oldestTweetCreatedAt)
-        this.queryTime = Date.parse(data.queriedAt)
-        this.spanSeconds = (this.queryTime - this.oldestTweetCreatedAt) / 1000
-        this.tweetsPerSecond = data.tweets.length / this.spanSeconds
-        
-        this.emoji = ''
+        this.score = data.score
         this.tweetPlurality = data.tweets.length !== 1 ? 'tweets' : 'tweet'
         this.timeQty = 0
         this.timeUnits = ''
-
-        this.possibleViews = data.metricSummary.possibleViews
-
-        if(this.spanSeconds < 60){
-            this.emoji = '🚀'
-            this.timeQty = Math.round(this.spanSeconds)
-            this.timeUnits = 's'
-        } else if (this.spanSeconds < 60 * 60) {
-            this.emoji = '🔥'
-            this.timeQty = Math.round(this.spanSeconds / 60) + 1
-            this.timeUnits = 'min'
-        } else if (this.spanSeconds < 60 * 60 * 24) {
-            this.emoji = '😐'
-            this.timeQty = Math.round(this.spanSeconds / (60 * 60)) + 1
-            this.timeUnits = 'hrs'
-        } else {
-            this.emoji = '🥶'
-            this.timeQty = Math.round(this.spanSeconds / (60 * 60 * 24)) + 1
-            this.timeUnits = 'days'
-        }
 
         this.isComplete = true
         this.isSuccess = !this.objectIsEmpty(this.data)
